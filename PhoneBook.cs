286ceea4f9650c40e867d688cb9a41bef5a95770
WilliamKinzig <wippawill2@gmail.com>
2018-07-27 17:55:45 -0700

    basic layout and specs.

---
+++ PhoneBook.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+
+class PhoneBook
+{
+    public static Dictionary<string, string> phoneNumbers = new Dictionary<string, string>();
+
+    static void Main()
+    {
+
+    }
+
+    static void AddContact()
+    {
+
+    }
+
+    static void LookUpContact()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+allows the user to input names and phone numbers
+
+allow the user to retrieve contact information
+
+mcs PhoneBook.cs
+mono PhoneBook.exe
